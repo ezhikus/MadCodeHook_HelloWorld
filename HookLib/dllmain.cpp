a7544c76b1970ead8f3227383129cf8b1243c87e
tangro <tangrofromhabr@gmail.com>
2014-02-19 00:03:51 +0200

    hook lib

--- HookLib/dllmain.cpp
+++ HookLib/dllmain.cpp
@@ -1,18 +1,22 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#include "madCHook.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+LRESULT (WINAPI * TrueSendMessageW)(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) = SendMessageW;
+
+__declspec(dllexport) LRESULT WINAPI MySendMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	if (Msg == WM_SETTEXT && wcsstr((LPCTSTR)lParam, L"Хабрахабр") != NULL)
+		return TrueSendMessageW(hWnd, Msg, wParam, (LPARAM)L"Привет, Хабр!");
+
+	return TrueSendMessageW(hWnd, Msg, wParam, lParam);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+		case DLL_PROCESS_ATTACH:
+			HookAPI("User32.dll", "SendMessageW", MySendMessageW, (PVOID*) &TrueSendMessageW);
 	}
 	return TRUE;
 }
